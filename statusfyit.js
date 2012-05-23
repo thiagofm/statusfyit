07f225de70d714f5075f8d465b9953ba6e09d298
Thiago Fernandes Massa <thiagown@gmail.com>
2012-05-22 22:58:44 -0300

    I'm having a bug, help me -- meteor list

--- statusfyit.js
+++ statusfyit.js
@@ -1,19 +1,42 @@
+// Collections
+Users = new Meteor.Collection("users");
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to statusfyit.";
+  $().ready(function(){
+      hello = Meteor.ui.render(function(){
+        return Template.hello();
+      });
+      $('body').html(hello);
+  });
+
+  //TEMPLATE HELLO
+  Template.hello.users = function () {
+    return Users.find({});
   };
 
   Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    'click #new_user_but' : function () {
+      new_user = Meteor.ui.render(function(){
+        return Template.new_user();
+      });
+      $('body').html(new_user);
     }
   };
+
+  //TEMPLATE NEW_USER
+  Template.new_user.events = {
+    'click #send' : function () {
+      Users.insert({name: $('#username').val()});
+      hello = Meteor.ui.render(function(){
+        return Template.hello();
+      });
+      $('body').html(hello);      
+    }
+  };
+
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
